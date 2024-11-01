4a56f8da3aa128eedda17c4cb8cec3b992e76eda
eSeohyeon <leeseohyeon1203@gmail.com>
2024-11-01 13:54:15 +0900

    1

--- app/src/main/java/com/example/oncare_front/LoginActivity.java
+++ app/src/main/java/com/example/oncare_front/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.oncare_front;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edit_email, edit_pwd;
+    Button btn_login;
+    ImageView btn_sign;
+    String email_input, pwd_input; // 사용자가 입력한 이메일과 비밀번호를 받는 문자열
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,27 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edit_email=findViewById(R.id.edit_email);
+        edit_pwd=findViewById(R.id.edit_pwd);
+        btn_login=findViewById(R.id.btn_login);
+        btn_sign=findViewById(R.id.btn_sign);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // 로그인 버튼 클릭
+                email_input= String.valueOf(edit_email.getText());
+                pwd_input= String.valueOf(edit_pwd.getText());
+            }
+        });
+        
+        btn_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { // 회원가입 버튼 클릭
+                
+            }
+        });
+
 
     }
 }
